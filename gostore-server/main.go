8038caa5edc97baa32f7fed8befbc1be6aede053
glaukiol1 <glaukiol1@gmail.com>
2022-04-03 21:29:36 +0200

    basic menu handling

--- gostore-server/main.go
+++ gostore-server/main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-	"fmt"
-
 	menu "github.com/glaukiol1/go-arrow-menu"
+	menuhandlers "github.com/glaukiol1/go-remote-storage/gostore-server/menu-handlers"
 )
 
 func _main(choice int) {
-	fmt.Print("Choice: " + fmt.Sprint(choice))
+	switch choice {
+	case 0:
+		menuhandlers.CreateServer()
+	case 1:
+		menuhandlers.StartServer()
+	case 2:
+		menuhandlers.ServerStatus()
+	case 3:
+		menuhandlers.ReviewConfig()
+	}
 }
 
 func main() {
 	options := []string{
 		"Create A Server",
 		"Start your current server",
+		"Server status",
 		"Review your config file",
 	}
 	menu.StartMenu(options, _main)
---
+++ gostore-server/menu-handlers/startServer.go
@@ -0,0 +1,5 @@
+package menuhandlers
+
+func StartServer() {
+
+}
